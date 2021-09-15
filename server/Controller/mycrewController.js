4f7aef564c42d2c2ccdaf2342fbdc5238f6b54b6
bonschicken <wjswls45645697@gmail.com>
2021-09-16 00:47:48 +0900

    [server](#42)/ feat/loginToken: 로그인 token적용
    mycrew api도 마찬가지로 toekn 적용

--- server/Controller/mycrewController.js
+++ server/Controller/mycrewController.js
@@ -1,1 +1,30 @@
-// getMycrew, exitMycrew, deleteMycrew
+const models = require("../models")
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  sendAccessToken,
+  saveRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+  decodeToken,
+  destroyToken,
+} = require("./Functions/tokenFunction")
+
+module.exports= {
+    getMycrew: async (req,res)=>{
+        
+        let accessToken = verifyAccessToken(req)
+        if(!accessToken) {
+            accessToken = req.cookies.accessToken;
+            const userId = decodeToken(accessToken).payload.id    
+            const refreshToken = verifyRefreshToken(userId)
+            if(!refreshToken) return res.status(401).json({message: "다시 로그인 해주세요"})            
+            accessToken = generateAccessToken(userId)
+        }
+        
+        
+        sendAccessToken(res,accessToken)
+
+        return res.status(200).json({message:"크루페이지로~"})
+    },
+}
