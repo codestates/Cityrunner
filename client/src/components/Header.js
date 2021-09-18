a640dd83505c664b0dbd9bbc32466f0797c77a3e
jmean12 <ooyhy123aa@gmail.com>
2021-09-18 15:46:38 +0900

    header 로그인시 ui 임시

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,82 +4,113 @@ import { theme } from "../themes/theme";
 import { Signup } from "./Modal/Signup";
 import { LoginModal } from "./Modal/LoginModal";
 import { Link } from "react-router-dom";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Container = styled.header`
-  position: fixed;
-  width: 100%;
-  color: white;
-  background-color: ${theme.color.black};
-  border-bottom: 1px solid ${theme.line.gray};
-  display: flex;
-  justify-content: space-between;
+	position: fixed;
+	width: 100%;
+	color: white;
+	background-color: ${theme.color.black};
+	border-bottom: 1px solid ${theme.line.gray};
+	display: flex;
+	justify-content: space-between;
 `;
 
 const RightSide = styled.div`
-  display: flex;
-  margin-right: 4rem;
-  .login-btn {
-    cursor: pointer;
-    margin-right: 2rem;
-    :hover {
-      color: ${theme.color.hovergray};
-      transition: 0.1s;
-    }
-  }
+	display: flex;
+	margin-right: 4rem;
+	.login-btn {
+		cursor: pointer;
+		margin-right: 2rem;
+		:hover {
+			color: ${theme.color.hovergray};
+			transition: 0.1s;
+		}
+	}
+	.logout-btn {
+		cursor: pointer;
+		margin-right: 2rem;
+		:hover {
+			color: ${theme.color.hovergray};
+			transition: 0.1s;
+		}
+	}
+	.icons {
+		cursor: pointer;
+		margin-right: 2rem;
+		margin-top: 0.8rem;
+		:hover {
+			color: ${theme.color.hovergray};
+			transition: 0.1s;
+		}
+	}
 `;
 
 const LeftSide = styled.div`
-  margin-left: 3rem;
+	margin-left: 3rem;
 `;
 
 export const Header = () => {
-  const [showSignupModal, setShowSignupModal] = useState(false);
-  const handleSignupModal = () => {
-    setShowSignupModal(!showSignupModal);
-  };
-  const [isModal, setisModal] = useState(false);
-  const handleModal = () => {
-    setisModal(!false);
-  };
-  const handleCloseModal = () => {
-    setisModal(false);
-  };
+	const [showSignupModal, setShowSignupModal] = useState(false);
+	//임시 로그인시 상태 useState
+	const [Exlogin, setExlogin] = useState(false);
 
-  useEffect(() => {
-    const close = (e) => {
-      if (e.keyCode === 27) {
-        handleCloseModal();
-      }
-    };
-    window.addEventListener("keydown", close);
-    return () => window.removeEventListener("keydown", close);
-  }, []);
+	const handleSignupModal = () => {
+		setShowSignupModal(!showSignupModal);
+	};
+	const [isModal, setisModal] = useState(false);
+	const handleModal = () => {
+		setisModal(!false);
+	};
+	const handleCloseModal = () => {
+		setisModal(false);
+	};
 
-  return (
-    <>
-      <Container>
-        <LeftSide>
-    					<Link to="/">
-          <h3>CityRunner</h3>
-    					</Link>
-        </LeftSide>
-        <RightSide>
-          <h4 className="login-btn" onClick={handleModal}>
-            로그인
-          </h4>
-          <h4 className="login-btn" onClick={handleSignupModal}>
-            회원가입
-          </h4>
-        </RightSide>
-      </Container>
-      <div onClick={handleCloseModal}>
-        {isModal ? <LoginModal></LoginModal> : null}
-      </div>
-      <div onClick={handleSignupModal}>
-        {showSignupModal ? (
-          <Signup setShowSignupModal={setShowSignupModal}></Signup>
-        ) : null}
-      </div>
-    </>
-  );
+	useEffect(() => {
+		const close = (e) => {
+			if (e.keyCode === 27) {
+				handleCloseModal();
+			}
+		};
+		window.addEventListener("keydown", close);
+		return () => window.removeEventListener("keydown", close);
+	}, []);
+
+	return (
+		<>
+			<Container>
+				<LeftSide>
+					<Link to="/">
+						<h3>CityRunner</h3>
+					</Link>
+				</LeftSide>
+				<RightSide>
+					{!Exlogin ? (
+						<>
+							{/* <h4 className="login-btn">매칭페이지</h4>
+							<h4 className="login-btn">마이페이지</h4> */}
+							<h4 className="login-btn" onClick={handleModal}>
+								로그인
+							</h4>
+							<h4 className="login-btn" onClick={handleSignupModal}>
+								회원가입
+							</h4>
+						</>
+					) : (
+						<h4 className="login-btn">로그아웃</h4>
+						//<FontAwesomeIcon className="icons" icon={faBars} size="2x" />
+					)}
+				</RightSide>
+			</Container>
+			<div onClick={handleCloseModal}>
+				{isModal ? <LoginModal></LoginModal> : null}
+			</div>
+			<div onClick={handleSignupModal}>
+				{showSignupModal ? (
+					<Signup setShowSignupModal={setShowSignupModal}></Signup>
+				) : null}
+			</div>
+		</>
+	);
 };
