0ba3bde484954265f9a97ce2e41d4f2b5d97972d
bonschicken <wjswls45645697@gmail.com>
2021-09-23 20:43:19 +0900

    [client] Fix/dev: dev 변수 이름 오타 수정

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,19 +2,19 @@ import React from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { MainFirst } from "../feature/main/MainFirst";
-import { MainFourth } from "../feature/main/MainFourth";
+import { MainFourth } from "../feature/main/Mainfourth";
 import { MainSecond } from "../feature/main/MainSecond";
-import { MainThird } from "../feature/main/MainThird";
+import { MainThird } from "../feature/main/Mainthird";
 
 export const Main = () => {
-	return (
-		<>
-			<Header />
-			<MainFirst />
-			<MainSecond />
-			<MainThird />
-			<MainFourth />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <MainFirst />
+      <MainSecond />
+      <MainThird />
+      <MainFourth />
+      <Footer />
+    </>
+  );
 };
