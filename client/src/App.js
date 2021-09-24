0bba0ce379b6ce441eec4c842f085d0da0b3c75d
Alamarama <pian14q@gmail.com>
2021-09-24 19:59:03 +0900

    feat2

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,26 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Main } from "./pages/Main";
 import { GlobalStyles } from "./themes/GlobalStyles";
-import { Mypage } from "./components/Mypage/Mypage"
+import { Mypage } from "./components/Mypage/Mypage";
 import { Matching } from "./pages/Matching";
 
-
 function App() {
-	return (
-		<Router>
-			<GlobalStyles />
-			<Switch>
-				<Route exact path="/">
-					<Main />
-				</Route>
-
-				<Route exact path="/Mypage">
-					<Mypage />
-
-				<Route exact path="/Matching">
-					<Matching />
-
-				</Route>
-			</Switch>
-		</Router>
-	);
+  return (
+    <Router>
+      <GlobalStyles />
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route exact path="/Mypage">
+          <Mypage />
+        </Route>
+        <Route exact path="/Matching">
+          <Matching />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
