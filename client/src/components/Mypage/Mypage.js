fbb0c4df7fc7cb7a3b1d599919be69c3ca7c00a2
jmean12 <ooyhy123aa@gmail.com>
2021-09-26 10:08:26 +0900

    formik기능 추가

--- client/src/components/Mypage/Mypage.js
+++ client/src/components/Mypage/Mypage.js
@@ -1,55 +1,57 @@
-import {useState} from 'react'
-import axios from 'axios'
-import './Mypage.css'
+import { useState } from "react";
+import axios from "axios";
+import "./Mypage.css";
 
-async function postImage({image, description}) {
-	const formData =new FormData();
-	formData.append("image", image)
-	formData.append("description",description)
+async function postImage({ image, description }) {
+	const formData = new FormData();
+	formData.append("image", image);
+	formData.append("description", description);
 
-	const result = await axios.post('http://localhost:4000/images',formData,{headers:{'Content-Type' : 'multipart/form-data'}})
-	console.log(result.data)
-	return result.data
+	const result = await axios.post("http://localhost:4000/images", formData, {
+		headers: { "Content-Type": "multipart/form-data" },
+	});
+	console.log(result.data);
+	return result.data;
 }
 
+export const Mypage = () => {
+	const [file, setFile] = useState();
+	const [description, setDescription] = useState("");
+	const [images, setImages] = useState([]);
 
-export const Mypage =() => {
+	const submit = async (event) => {
+		event.preventDefault();
+		const result = await postImage({ image: file, description });
 
-  const [file,setFile] =useState()
-  const [description,setDescription] =useState("")
-  const [images,setImages] = useState([])
+		setImages([result.image, ...images]);
+	};
 
-  const submit = async event => {
-	event.preventDefault()
-	const result = await postImage({image: file, description})
-	
-	setImages([result.image, ...images])
-  };
+	const fileSelected = (event) => {
+		const file = event.target.files[0];
+		setFile(file);
+	};
 
-  const fileSelected = event => {
-    const file = event.target.files[0]
-	setFile(file)
-  };
+	return (
+		<>
+			<div className="App">
+				<form onSubmit={submit}>
+					<input onChange={fileSelected} type="file" accept="image/*"></input>
+					<input
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
+						type="text"
+					></input>
+					<button type="submit">Submit</button>
+				</form>
 
-
-  return (
-    <>
-      <div className="App">
-        <form onSubmit={submit}>
-          <input onChange={fileSelected} type="file" accept="image/*"></input>
-          <input value={description} onChange={e => setDescription(e.target.value)} type="text"></input>
-          <button type="submit">Submit</button>
-        </form>
-
-    {/* { images.map( image => (
+				{/* { images.map( image => (
         <div key={image}>
         <img src={image}></img>
         </div>
     ))} */}
 
-        <img src="http://localhost:4000/images/38f550d87d82bbd67f3a236aabf31e9a"></img>
-      </div>
-    </>
-  );
- };
-
+				<img src="http://localhost:4000/images/38f550d87d82bbd67f3a236aabf31e9a"></img>
+			</div>
+		</>
+	);
+};
