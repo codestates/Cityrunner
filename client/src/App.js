77c6a604c728e89e436449e8726d475025850a89
Alamarama <pian14q@gmail.com>
2021-09-27 16:34:40 +0900

    feat2

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { Matching } from "./pages/Matching";
 import Mypage from "./pages/Mypage";
 
 function App() {
-<<<<<<< HEAD
   return (
     <Router>
       <GlobalStyles />
@@ -22,24 +21,6 @@ function App() {
       </Switch>
     </Router>
   );
-=======
-	return (
-		<Router>
-			<GlobalStyles />
-			<Switch>
-				<Route exact path="/">
-					<Main />
-				</Route>
-				<Route exact path="/Mypage">
-					<Mypage />
-				</Route>
-				<Route exact path="/Matching">
-					<Matching />
-				</Route>
-			</Switch>
-		</Router>
-	);
->>>>>>> cf9a4e47aa33dc75d6807ac9d5b1f229844c811a
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-<<<<<<< HEAD
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-=======
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./redux/combine";
@@ -22,15 +14,14 @@ import { theme } from "./themes/theme";
 const store = applyMiddleware(promiseMiddlerware, reduxThunk)(createStore);
 
 ReactDOM.render(
-	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<Provider store={store(rootReducer)}>
-				<App />
-			</Provider>
-		</ThemeProvider>
-	</React.StrictMode>,
-	document.getElementById("root")
->>>>>>> cf9a4e47aa33dc75d6807ac9d5b1f229844c811a
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Provider store={store(rootReducer)}>
+        <App />
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
