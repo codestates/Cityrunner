e1a3b84e5b4a1ea4b76867e2fd4e05506015f530
Alamarama <pian14q@gmail.com>
2021-09-29 18:05:32 +0900

    dev최신화

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,9 @@ import { GlobalStyles } from "./themes/GlobalStyles";
 import { Socketio } from "./components/Socketio/Socketio";
 import { Mypage } from "./components/Mypage/Mypage";
 import { Matching } from "./pages/Matching";
-import Mypage from "./pages/Mypage";
 import { MyRoom } from "./components/modal/MyRoom";
 
 function App() {
-<<<<<<< HEAD
-=======
-
->>>>>>> 92e9197cb7d7ee9d4f2f3d219f11214765ad062e
   return (
     <Router>
       <GlobalStyles />
@@ -25,22 +20,15 @@ function App() {
         <Route exact path="/Matching">
           <Matching />
         </Route>
-<<<<<<< HEAD
-      </Switch>
-    </Router>
-  );
-=======
         <Route exact path="/Chating">
           <Socketio />
         </Route>
-        <Route exact path="/MyRoom">  
-					<MyRoom />
-				</Route>
+        <Route exact path="/MyRoom">
+          <MyRoom />
+        </Route>
       </Switch>
     </Router>
   );
-
->>>>>>> 92e9197cb7d7ee9d4f2f3d219f11214765ad062e
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -12,30 +12,19 @@ import promiseMiddlerware from "redux-promise";
 import reduxThunk from "redux-thunk";
 
 const createStoreWidthMiddleware = applyMiddleware(
-	promiseMiddlerware,
-	reduxThunk
+  promiseMiddlerware,
+  reduxThunk
 )(createStore);
 
 ReactDOM.render(
-<<<<<<< HEAD
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Provider store={store(rootReducer)}>
+    <Provider store={createStoreWidthMiddleware(rootReducer)}>
+      <ThemeProvider theme={theme}>
         <App />
-      </Provider>
-    </ThemeProvider>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
-=======
-	<React.StrictMode>
-		<Provider store={createStoreWidthMiddleware(rootReducer)}>
-			<ThemeProvider theme={theme}>
-				<App />
-			</ThemeProvider>
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById("root")
->>>>>>> 92e9197cb7d7ee9d4f2f3d219f11214765ad062e
 );
 
 // If you want to start measuring performance in your app, pass a function
