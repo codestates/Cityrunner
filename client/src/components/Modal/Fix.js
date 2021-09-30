0436a3e1cac0a35b7d42f50f993b92f9b6fa8983
Alamarama <pian14q@gmail.com>
2021-09-30 17:00:40 +0900

    mypage 버튼기능

---
+++ client/src/components/Modal/Fix.js
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { theme } from "../../themes/theme";
+import { flexCenter, flexColum } from "../../themes/flex";
+import axios from "axios";
+
+const ModalContainer = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  ${flexCenter}
+  background: rgba(0, 0, 0, 0.6);
+`;
+
+const Modal = styled.div`
+  width: 430px;
+  height: 500px;
+  padding: 1rem;
+  background: white;
+  border-radius: 2px;
+  h3 {
+    ${flexCenter}
+  }
+`;
+
+const Input = styled.div`
+  ${flexColum}
+  input {
+    height: 1.8rem;
+    width: 200px;
+    margin-top: -1rem;
+    border: solid 1px;
+    padding-left: 1rem;
+  }
+  h5 {
+    ${flexCenter}
+    margin-bottom:1.5rem;
+  }
+`;
+
+const Title = styled.h2`
+  ${flexCenter}
+  margin-top: 2rem;
+  margin-bottom: 3rem;
+`;
+
+const Btn = styled.div`
+  ${flexColum}
+  margin-top: 1.5rem;
+  button {
+    height: 1.5rem;
+    width: 200px;
+    height: 2rem;
+    margin: 0.5rem;
+    margin-top: 4rem;
+    background-color: ${theme.color.black};
+    color: white;
+    font-weight: bold;
+    :hover {
+      background-color: white;
+      color: ${theme.color.black};
+      border: solid 1px;
+      transition: 0.4s;
+    }
+  }
+`;
+
+export const Fix = (props) => {
+  const [FixInfo, setFixInfo] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const CloseFix = () => {
+    props.setShowFixModal(false);
+  };
+
+  const OnClick = (key) => (e) => {
+    setFixInfo({ ...FixInfo, [key]: e.target.value });
+  };
+  const FixButton = () => {
+    if (!FixInfo.email && !FixInfo.password && !FixInfo.username) {
+      alert("모든 값을 입력해주세요.");
+    }
+    axios
+      .post(`http://localhost:4000/user/signup`, FixInfo, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          CloseFix();
+          alert("회원가입에 성공하였습니다.");
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 409) {
+          alert("이미 존재하는 이메일,닉네임 입니다");
+          return;
+        }
+      });
+  };
+  return (
+    <>
+      <ModalContainer>
+        <Modal onClick={(e) => e.stopPropagation()}>
+          <Title>회원 정보 수정</Title>
+          <Input>
+            <h5>현재 비밀번호</h5>
+            <input
+              name="email"
+              type="text"
+              placeholder="Email Adress"
+              onChange={OnClick("email")}
+            />
+            <h5>새 비밀번호</h5>
+            <input
+              name="password"
+              type="password"
+              placeholder="6자리 이상 입력해주세요."
+              onChange={OnClick("password")}
+            />
+            <h5>닉네임</h5>
+            <input
+              name="nickname"
+              type="text"
+              placeholder="닉네임"
+              onChange={OnClick("username")}
+            />
+          </Input>
+          <Btn>
+            <button onClick={FixButton}>수정하기</button>
+          </Btn>
+        </Modal>
+      </ModalContainer>
+    </>
+  );
+};
