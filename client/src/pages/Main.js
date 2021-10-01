5699ca7ed38cbde51b3d48acdcaf0788cf6abfad
jmean12 <ooyhy123aa@gmail.com>
2021-10-01 17:18:24 +0900

    Login 상태유지 및 관리 구현

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
@@ -8,13 +8,15 @@ import { MainFirst } from "../components/main/MainFirst";
 import { MainFourth } from "../components/main/MainFourth";
 import { MainSecond } from "../components/main/MainSecond";
 import { MainThird } from "../components/main/MainThird";
-//import { loginUser } from "../redux/modules/user";
+
 import qs from "querystring";
 import { useHistory } from "react-router";
+import { loginUser } from "../redux/modules/user";
 
 export const Main = () => {
 	const dispatch = useDispatch();
 	const history = useHistory();
+	const [state, setState] = useState(false);
 
 	useEffect(async () => {
 		const url = new URL(window.location.href);
@@ -29,7 +31,9 @@ export const Main = () => {
 				})
 				.then(async (res) => {
 					const { email, password } = res.data.data;
-					//dispatch(loginUser({ email, password }));
+					dispatch(loginUser({ email, password }));
+					localStorage.setItem("userinfo", JSON.stringify({ email, password }));
+					setState(true);
 					history.push("/");
 				})
 				.catch((err) => console.log(err));
@@ -57,13 +61,14 @@ export const Main = () => {
 						})
 						.then((res) => {
 							const { email, password } = res.data.data;
-							//dispatch(loginUser({ email, password }));
+							dispatch(loginUser({ email, password }));
 							history.push("/");
 						})
 						.catch((err) => console.log(err));
 				});
 		}
-	}, []);
+	}, [state]);
+
 	return (
 		<>
 			<Header />
