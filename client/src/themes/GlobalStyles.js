2650206da389c3e33a6e484726e8a3a78fd367a7
bonschicken <wjswls45645697@gmail.com>
2021-10-06 13:07:31 +0900

    글꼴 추가

--- client/src/themes/GlobalStyles.js
+++ client/src/themes/GlobalStyles.js
@@ -2,11 +2,20 @@ import { createGlobalStyle } from "styled-components";
 import { theme } from "./theme";
 
 export const GlobalStyles = createGlobalStyle`
+
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
 	* {
     border: none;
     outline: none;
     list-style: none;
     text-decoration: none;
+	font-family: 'Pretendard-Regular'; 
+	
   }
   button{
     cursor: pointer;
@@ -30,7 +39,9 @@ export const GlobalStyles = createGlobalStyle`
 		html,
 body{
 width:100%;
+
 overflow-x:hidden;
+
 }
 	}
 `;
