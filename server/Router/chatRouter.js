aa646d521c68d0e18ea633d4421936f1dc9766c5
bonschicken <wjswls45645697@gmail.com>
2021-10-11 01:08:10 +0900

    main페이지 최종본

--- server/Router/chatRouter.js
+++ server/Router/chatRouter.js
@@ -1,17 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createAndSendMessage,
-  getMessage,
-  deleteMessage,
-  enterMessage,
-  reportUser,
-} = require("../Controller/chatController");
+const { getMessage } = require("../Controller/chatController");
 
-router.post("/:postId", createAndSendMessage);
 router.get("/", getMessage);
-router.delete("/:postId", deleteMessage);
-//router.get('/:postId', enterMessage);
-router.post("/:postId/:userid", reportUser);
 
 module.exports = router;
